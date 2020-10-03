bbda4103a5390d0612e5d124c7574e75e2737fc0
Harsh Shandilya <me@msfjarvis.dev>
2020-10-03 16:35:59 +0530

    ControlsService: rewrite and simplify
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/powertiles/ControlsService.kt
+++ app/src/main/java/dev/msfjarvis/powertiles/ControlsService.kt
@@ -8,7 +8,6 @@ import android.provider.Settings
 import android.service.controls.Control
 import android.service.controls.ControlsProviderService
 import android.service.controls.DeviceTypes
-import android.service.controls.actions.BooleanAction
 import android.service.controls.actions.ControlAction
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.processors.ReplayProcessor
@@ -16,24 +15,36 @@ import org.reactivestreams.FlowAdapters
 import java.util.concurrent.Flow
 import java.util.function.Consumer
 
+private const val CONTROL_REQUEST_CODE = 100
+private const val CONTROL_ID_SETTINGS = "settings_control"
+
 class ControlsService : ControlsProviderService() {
+    private val controls = hashMapOf<String, Control>()
     private lateinit var updatePublisher: ReplayProcessor<Control>
     private val settingsPendingIntent by lazy {
         val context: Context = baseContext
         val i = Intent(Settings.ACTION_SETTINGS).apply {
-            setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
         PendingIntent.getActivity(
             context,
             CONTROL_REQUEST_CODE,
             i,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
     }
 
-    override fun createPublisherForAllAvailable(): Flow.Publisher<Control> {
-        val controls = mutableListOf<Control>()
-        val control = Control.StatefulBuilder(CONTROL_BUTTON_ID, settingsPendingIntent)
+    override fun onCreate() {
+        super.onCreate()
+        makeDefaultControls()
+    }
+
+    private fun makeDefaultControls() {
+        controls[CONTROL_ID_SETTINGS] = makeSettingsControl()
+    }
+
+    private fun makeSettingsControl(): Control {
+        return Control.StatefulBuilder(CONTROL_ID_SETTINGS, settingsPendingIntent)
             .setTitle(getString(R.string.settings_button_title))
             .setSubtitle(getString(R.string.settings_button_summary))
             .setStructure(getString(R.string.structure_home))
@@ -41,8 +52,11 @@ class ControlsService : ControlsProviderService() {
             .setDeviceType(DeviceTypes.TYPE_SWITCH)
             .setStatus(Control.STATUS_OK)
             .build()
-        controls.add(control)
-        return FlowAdapters.toFlowPublisher(Flowable.fromIterable(controls))
+    }
+
+    override fun createPublisherForAllAvailable(): Flow.Publisher<Control> {
+        makeDefaultControls()
+        return FlowAdapters.toFlowPublisher(Flowable.fromIterable(controls.map { it.value }))
     }
 
     override fun performControlAction(
@@ -50,40 +64,17 @@ class ControlsService : ControlsProviderService() {
         action: ControlAction,
         consumer: Consumer<Int>
     ) {
-        if (action is BooleanAction) {
-            consumer.accept(ControlAction.RESPONSE_OK)
-            val control = Control.StatefulBuilder(CONTROL_BUTTON_ID, settingsPendingIntent)
-                .setTitle(getString(R.string.settings_button_title))
-                .setSubtitle(getString(R.string.settings_button_summary))
-                .setStructure(getString(R.string.structure_home))
-                .setCustomIcon(Icon.createWithResource(this, R.drawable.ic_outline_settings_24))
-                .setDeviceType(DeviceTypes.TYPE_SWITCH)
-                .setStatus(Control.STATUS_OK)
-                .build()
-            updatePublisher.onNext(control)
+        when (controlId) {
+            CONTROL_ID_SETTINGS -> {
+                controls[CONTROL_ID_SETTINGS] = makeSettingsControl()
+                updatePublisher.onNext(controls[CONTROL_ID_SETTINGS])
+            }
         }
     }
 
     override fun createPublisherFor(controlIds: MutableList<String>): Flow.Publisher<Control> {
+        makeDefaultControls()
         updatePublisher = ReplayProcessor.create()
-        val controls = mutableListOf<Control>()
-        if (controlIds.contains(CONTROL_BUTTON_ID)) {
-            val control = Control.StatefulBuilder(CONTROL_BUTTON_ID, settingsPendingIntent)
-                .setTitle(getString(R.string.settings_button_title))
-                .setSubtitle(getString(R.string.settings_button_summary))
-                .setStructure(getString(R.string.structure_home))
-                .setCustomIcon(Icon.createWithResource(this, R.drawable.ic_outline_settings_24))
-                .setDeviceType(DeviceTypes.TYPE_SWITCH)
-                .setStatus(Control.STATUS_OK)
-                .build()
-            updatePublisher.onNext(control)
-            controls.add(control)
-        }
-        return FlowAdapters.toFlowPublisher(Flowable.fromIterable(controls))
-    }
-
-    companion object {
-        private const val CONTROL_REQUEST_CODE = 100
-        private const val CONTROL_BUTTON_ID = "button_id"
+        return FlowAdapters.toFlowPublisher(Flowable.fromIterable(controls.map { it.value }))
     }
 }
