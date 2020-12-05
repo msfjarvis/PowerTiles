d70d165fa725096cc69fb615810df46c2b3c25d8
Harsh Shandilya <me@msfjarvis.dev>
2020-12-05 12:45:33 +0530

    Add a tile for WiFi networks panel
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/powertiles/SettingsPanelTile.kt
+++ app/src/main/java/dev/msfjarvis/powertiles/SettingsPanelTile.kt
@@ -1,2 +1,14 @@
 package dev.msfjarvis.powertiles
 
+import android.content.Intent
+import android.provider.Settings
+import android.service.quicksettings.TileService
+
+class SettingsPanelTile : TileService() {
+    override fun onClick() {
+        super.onClick()
+        startActivityAndCollapse(Intent(Settings.Panel.ACTION_WIFI).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        })
+    }
+}
